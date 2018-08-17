e791c8bd2149a2fba990f4a47471fc6fcd7bdee4
Gabriel Lizarraga <41972694+Glizarraga44451@users.noreply.github.com>
2018-08-17 10:57:07 -0700

    Update projectemail.php

--- projectemail.php
+++ projectemail.php
@@ -1,7 +1,6 @@
 <?php
 if(!isset($_POST['submit']))
 {
-    //This page should not be accessed directly. Need to submit the form.
     echo "error; you need to submit the form!";
 }
 $name = $_POST['name'];
